4c77ea64889d4280be225c846c18582814d4bc89
gamedina0317 <63561567+gamedina0317@users.noreply.github.com>
2020-05-29 19:21:53 -0500

    Update Categoria.java

--- Nuevo Perfil/Categoria.java
+++ Nuevo Perfil/Categoria.java
@@ -1,22 +1,22 @@
 package com.example.prueba;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class Categoria extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String seleccion="";
-    String finale;
+    String finale;//Esta es la variable importante, el string a partil del cual se hace la busqueda
     ArrayList <arraylists> lista = new ArrayList<arraylists>();
     String[] elementos={"Comida","Micelania","Accesorios"};
     String[] comida={"Almuerzo","Mecato","Postres","Panaderia"};
@@ -35,9 +35,11 @@ public class Categoria extends AppCompatActivity implements AdapterView.OnItemSe
             @Override
             public void onClick(View v) {
                 Intent intento=new Intent(getApplicationContext(),Mapa.class);
+                //Un ciclo o lo que sea que pueda extraer esos 3 datos en una lista para pasala a la siguiente
                 lista.add(new arraylists(4.636404559796773,-74.08373344648129,"1"));
                 lista.add(new arraylists(4.635666692168087,-74.08373344648129,"2"));
                 lista.add(new arraylists(4.635329839242644,-74.08289916830648,"3"));
+                //Otro ciclo o cualquier cosa que filtre y elimine los resultados que esten en las coordenadas por default que le coloquemos
                 intento.putExtra("cordenadas",lista);
                 intento.putExtra("estado",true);
                 System.out.println("Lo va a sacar");
@@ -67,7 +69,7 @@ public class Categoria extends AppCompatActivity implements AdapterView.OnItemSe
                 @Override
                 public void onClick(View v) {
                     for(int x=0;x<cajas.size();x++){
-                        cajas.get(x).setChecked(false);
+                        cajas.get(x).setChecked(false);//ESTO ES PARA QUE DESCHEQUEE TODAS LAS CAJAS QUE ESTEN CHEQUEADAS
                     }
                     cajas.get(seleccion).setChecked(true);
                     finale=cajas.get(seleccion).getText().toString();
@@ -80,7 +82,9 @@ public class Categoria extends AppCompatActivity implements AdapterView.OnItemSe
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         seleccion = tipo1.getSelectedItem().toString();
-        finale="";
+        for(int x=0;x<cajas.size();x++){
+            cajas.get(x).setChecked(false);//ESTO ES PARA QUE DESCHEQUEE TODAS LAS CAJAS QUE ESTEN CHEQUEADAS
+        }
         if(seleccion.equals(elementos[0])){
             this.mostrar(comida);
         }
@@ -95,7 +99,7 @@ public class Categoria extends AppCompatActivity implements AdapterView.OnItemSe
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
-    }
+    }//Es para que muestre solamente las cajas que si tienen algo adentro
     public void mostrar(String[] lista){
         for (int i=0; i<cajas.size();i++){
             try{
@@ -108,3 +112,4 @@ public class Categoria extends AppCompatActivity implements AdapterView.OnItemSe
         }
     }
 }
+
