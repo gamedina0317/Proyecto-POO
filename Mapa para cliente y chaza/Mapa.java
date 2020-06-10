63dfa676098b76921e98cea524b7382ab7398074
gamedina0317 <63561567+gamedina0317@users.noreply.github.com>
2020-06-09 22:21:31 -0500

    Update Mapa.java
    Este es e mapa final

--- Mapa para cliente y chaza/Mapa.java
+++ Mapa para cliente y chaza/Mapa.java
@@ -1,13 +1,21 @@
 package com.example.prueba;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.multidex.MultiDex;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -17,20 +25,31 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallback {
-
+public class Mapa extends FragmentActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapActivity";
+    private static final int ACCESS_LOCATION_REQUEST_CODE = 9004;
     private GoogleMap mMap;
 
     ArrayList<arraylists> lugares;
     boolean estado;
 
     @Override
+    protected void attachBaseContext(Context Base) {
+        super.attachBaseContext(Base);
+        MultiDex.install(this);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent intento=getIntent();
-        this.estado=(boolean)intento.getBooleanExtra("estado",true);
-        this.lugares = (ArrayList<arraylists>)intento.getSerializableExtra("cordenadas");
+        Intent intento = getIntent();
+        this.estado = (boolean) intento.getBooleanExtra("estado", true);
+        this.lugares = (ArrayList<arraylists>) intento.getSerializableExtra("cordenadas");
         super.onCreate(savedInstanceState);
         System.out.println("ya tiene la lista");
+        LatLng inicial = new LatLng(4.63874255, -74.08373344648129);
+
+        //moveCamera(inicial,15);
+        System.out.println("ya carga la vista inicial");
         setContentView(R.layout.activity_mapa);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -50,14 +69,28 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        lugares.add(new LatLng(4.637862702334999,-74.08292440664117));
+
+        System.out.println("ya esta en el on readdy y ya tiene el permiso");
+        mMap = googleMap;
+        LatLng unal = new LatLng(4.63874255d, -74.085237757545d);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(unal,15f));
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            getUserLocation();
+        } else{
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+            } else{
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_LOCATION_REQUEST_CODE);
+            }
+        }
+        /*lugares.add(new LatLng(4.637862702334999,-74.08292440664117));
         lugares.add(new LatLng(4.636404559796773,-74.08373344648129));
         lugares.add(new LatLng(4.635666692168087,-74.08301450329569));
         lugares.add(new LatLng(4.635329839242644,-74.08289916830648));
         lugares.add(new LatLng(4.635420796834211,-74.08253700911179));*/
         System.out.println("Llego hasta el mapa");
-        if(estado==true) {//para los clientes
-            Button boton= (Button)findViewById(R.id.boton);
+        if (estado == true) {//para los clientes
+            Button boton = (Button) findViewById(R.id.boton);
             boton.setVisibility(View.INVISIBLE);
             for (int i = 0; i < lugares.size(); i++) {
                 /*LatLng pos=new LatLng(lugares.get(i)[0],lugares.get(i)[1]);*/
@@ -71,17 +104,16 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
                         Intent Intento = new Intent(getApplicationContext(), Perfilchaza.class);
                         int posicion = (int) marker.getTag();
                         Intento.putExtra("Nombre", lugares.get(posicion).getId());
-                        Intento.putExtra("estado",true);
+                        Intento.putExtra("estado", true);
                         startActivity(Intento);
                         return true;
                     }
                 });
             }
-        }
-        else{//para cuando son chazas
+        } else {//para cuando son chazas
 
             /*LatLng pos=new LatLng(lugares.get(i)[0],lugares.get(i)[1]);*/
-            final  Marker marcador = mMap.addMarker(new MarkerOptions().position(new LatLng(lugares.get(0).getLat(), lugares.get(0).getLng())).title("Marker in la Nacho").draggable(true));
+            final Marker marcador = mMap.addMarker(new MarkerOptions().position(new LatLng(lugares.get(0).getLat(), lugares.get(0).getLng())).title("Marker in la Nacho").draggable(true));
             System.out.println("crea los marcadores");
                 /*mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                     @Override
@@ -98,7 +130,8 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
                 });*/
 
             mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
-                LatLng temp=null;
+                LatLng temp = null;
+
                 @Override
                 public void onMarkerDragStart(Marker marker) {
 
@@ -106,7 +139,7 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
 
                 @Override
                 public void onMarkerDrag(Marker marker) {
-                    temp=marker.getPosition();
+                    temp = marker.getPosition();
                 }
 
                 @Override
@@ -114,14 +147,14 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
                     marker.setPosition(temp);
                 }
             });
-            Button boton= (Button)findViewById(R.id.boton);
+            Button boton = (Button) findViewById(R.id.boton);
             boton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent Intento = new Intent(getApplicationContext(), Perfilchaza.class);
                     Double lat = marcador.getPosition().latitude;
                     Double lon = marcador.getPosition().longitude;
-                    String pasa=lat+"  "+lon+"";
+                    String pasa = lat + "  " + lon + "";
                     Toast.makeText(getApplicationContext(), pasa, Toast.LENGTH_SHORT).show();
                     //Intento.putExtra("Nombre", pasa);
                     //Intento.putExtra("estado",false);
@@ -131,5 +164,20 @@ public class Mapa<lugares> extends FragmentActivity implements OnMapReadyCallbac
 
         }
 
+
+    }
+    private void getUserLocation(){
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == ACCESS_LOCATION_REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getUserLocation();
+            } else{
+                //Hacer mensaje :v
+            }
+        }
     }
 }
